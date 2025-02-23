1bdbfba5c7969420172dfb4d607e296583843efa
Kevon Kibochi <devokrann@gmail.com>
2025-02-23 21:08:31 +0300

    refactor: switch email reference

--- src/libraries/wrappers/email/contact.ts
+++ src/libraries/wrappers/email/contact.ts
@@ -1,4 +1,4 @@
-import appData from '@/data/app';
+import appData, { emails } from '@/data/app';
 import resend from '@/libraries/resend';
 import { EmailContactCreate, EmailInquiry } from '@/types/email';
 import { segmentFullName } from '@/utilities/formatters/string';
@@ -66,14 +66,12 @@ export const contactCreateWelcome = async (params: {
 }) => {
   const { data, error } = await resend.emails.send({
     from: `${appData.name.app} <${
-      isProduction()
-        ? process.env.NEXT_PUBLIC_EMAIL_NOREPLY!
-        : process.env.NEXT_RESEND_EMAIL!
+      isProduction() ? emails.noreply : process.env.NEXT_RESEND_EMAIL!
     }>`,
-    to: [isProduction() ? params.to : process.env.NEXT_PUBLIC_EMAIL_INFO!],
+    to: [isProduction() ? params.to : emails.info!],
     subject: `Welcome To ${appData.name.company} Newsletter`,
     html: await render(EmailOnboardNewsletter()),
-    replyTo: process.env.NEXT_PUBLIC_EMAIL_NOREPLY!,
+    replyTo: emails.noreply!,
   });
 
   if (!error) {
