7cabecc36a21931f0fb7094f6e151ad1fffb10c7
Kevon Kibochi <dev@kevon.net>
2025-05-13 15:43:55 +0300

    chore: add location data

--- src/data/app.ts
+++ src/data/app.ts
@@ -40,7 +40,7 @@ export const hours = {
 
 export const locations = {
   main: {
-    location: '410 Terry Ave. North, Seattle, WA 98109',
+    location: 'Westlands, Nairobi, KE 00100',
     pin: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.682238005543!2d36.810028700000004!3d-1.2681247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f173c0a1f9de7%3A0xad2c84df1f7f2ec8!2sWestlands%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1744114805984!5m2!1sen!2ske',
   },
 };
