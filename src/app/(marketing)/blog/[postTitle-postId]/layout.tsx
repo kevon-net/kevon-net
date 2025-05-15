87133cf5cda93082a8e486f39cac52406e4c5acc
Kevon Kibochi <dev@kevon.net>
2025-05-15 20:29:42 +0300

    feat: add blog listing page, update blog page layout files

--- src/app/(marketing)/blog/[postTitle-postId]/layout.tsx
+++ src/app/(marketing)/blog/[postTitle-postId]/layout.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import LayoutBody from '@/components/layout/body';
 import { typeParams } from '../layout';
-// import { Metadata } from 'next';
-// import { extractUuidFromParam } from '@/utilities/helpers/string';
+import { Metadata } from 'next';
+import { extractUuidFromParam } from '@/utilities/helpers/string';
+import { PostRelations } from '@/types/models/post';
+import { createClient } from '@/libraries/supabase/server';
 
-// export const generateMetadata = async ({
-//   params,
-// }: {
-//   params: typeParams;
-// }): Promise<Metadata> => {
-//   const postId = extractUuidFromParam(params['postTitle-postId']);
+export const generateMetadata = async ({
+  params,
+}: {
+  params: typeParams;
+}): Promise<Metadata> => {
+  const postId = extractUuidFromParam((await params)['postTitle-postId']);
 
-//   const { posts }: { posts: PostRelations[] } = await postsGet();
+  const supabase = await createClient();
 
-//   const post = posts.find((p) => p.id == postId);
+  const { data: posts } = await supabase.from('posts').select();
 
-//   return {
-//     title: post?.title,
-//     description: post?.excerpt,
-//     category: post?.category?.title,
-//   };
-// };
+  const post = ((posts || []) as PostRelations[]).find((p) => p.id == postId);
+
+  return {
+    title: post?.title,
+    description: post?.excerpt,
+    category: post?.category?.title,
+  };
+};
 
 export default function LayoutPost({
   children, // will be a page or nested layout
