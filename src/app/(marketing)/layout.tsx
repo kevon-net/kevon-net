ca5a2f562cdf084004b2e705fa8c2e996641eadb
Kevon Kibochi <dev@kevon.net>
2025-05-15 20:29:42 +0300

    feat: add blog listing page, update blog page layout files

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import LayoutBody from '@/components/layout/body';
 import AffixTop from '@/components/common/affixi/top';
 import ProviderStore from '@/components/providers/store';
-import NavbarMain from '@/components/layout/navbars/main';
-import FooterMain from '@/components/layout/footer/main';
 
 export default function LayoutMarketing({
   children, // will be a page or nested layout
@@ -12,7 +10,7 @@ export default function LayoutMarketing({
 }) {
   return (
     <ProviderStore>
-      <LayoutBody nav={<NavbarMain />} footer={<FooterMain />}>
+      <LayoutBody>
         <main>{children}</main>
 
         <AffixTop />
