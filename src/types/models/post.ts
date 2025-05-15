8c94dd2e3867cd3ddbadaccbc6899e7e37394251
Kevon Kibochi <dev@kevon.net>
2025-05-15 20:25:51 +0300

    feat: setup schema

---
+++ src/types/models/post.ts
@@ -0,0 +1,19 @@
+import { Prisma } from '@/generated/prisma';
+import { Post } from '@/generated/prisma';
+
+// Type for creating a item (without id and relations)
+export type PostCreate = Prisma.PostCreateInput;
+
+// Type for updating a item (all fields optional except id)
+export type PostUpdate = Prisma.PostUpdateInput;
+
+// Type for default item (with id and no relations)
+export type PostGet = Post;
+
+// Type for fetched item with relations
+export type PostRelations = Prisma.PostGetPayload<{
+  include: {
+    // _count: { select: { Comments: true } };
+    category: true;
+  };
+}>;
