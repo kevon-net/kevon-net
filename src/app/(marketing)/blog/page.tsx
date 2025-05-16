2ee2ada8eded81e8e58e6e6c1696b6f244b9db9c
Kevon Kibochi <dev@kevon.net>
2025-05-16 20:21:48 +0300

    refactor: setup server side data fetching

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import IntroPage from '@/components/layout/intros/page';
 import LayoutPage from '@/components/layout/page';
 import PartialBlogList from '@/components/partials/page/blog-list';
+import { postsGet } from '@/services/database/posts';
+import ErrorMain from '@/components/partials/errors/main';
+
+export const revalidate = 3600;
 
 export default async function Blog() {
+  const { data: posts, error } = await postsGet();
+
+  if (error) {
+    console.error(error);
+    return <ErrorMain />;
+  }
+
+  if (posts == null) {
+    console.error('Posts is null');
+    return <ErrorMain />;
+  }
+
   return (
     <LayoutPage>
       <IntroPage
@@ -13,7 +29,7 @@ export default async function Blog() {
         }}
       />
 
-      <PartialBlogList />
+      <PartialBlogList posts={posts} />
     </LayoutPage>
   );
 }
