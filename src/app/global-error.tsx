c91dbdaa4d8beb4c64ff62f6df774db04cf3c5f9
Kevon Kibochi <dev@kevon.net>
2025-05-16 20:22:10 +0300

    feat: setup generic error component

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Stack, Button, Flex, Group } from '@mantine/core';
-import LayoutSection from '@/components/layout/section';
-import { SECTION_SPACING } from '@/data/constants';
-import IntroPage from '@/components/layout/intros/page';
+import ErrorMain from '@/components/partials/errors/main';
 
 export default function GlobalError({
   // error,
@@ -16,29 +13,7 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <LayoutSection id={'page-not-found'}>
-          <Stack justify="center" mih={'100vh'} pb={SECTION_SPACING}>
-            <IntroPage
-              props={{
-                path: `500`,
-                title: 'Something Went Wrong!',
-                desc: `The page you are trying to open has triggered an error. Our servers seem to have run into a problem. You can try again later.`,
-              }}
-            />
-
-            <Group justify="center">
-              <Flex
-                direction={{ base: 'column', xs: 'row' }}
-                align={'center'}
-                gap={'md'}
-              >
-                <Button variant="light" onClick={() => reset()}>
-                  Try Again
-                </Button>
-              </Flex>
-            </Group>
-          </Stack>
-        </LayoutSection>
+        <ErrorMain reset={reset} />
       </body>
     </html>
   );
