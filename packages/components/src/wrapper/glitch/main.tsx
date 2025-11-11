230253c058001ca3aa38155f66371f9c15ff1afa
Kevon <dev@kevon.net>
2025-11-11 13:50:32 +0300

    refactor: update glitch styles

--- packages/components/src/wrapper/glitch/main.tsx
+++ packages/components/src/wrapper/glitch/main.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './main.module.scss';
 import { Box, BoxProps } from '@mantine/core';
 
-export default function Main({
-  props,
-  ...restProps
-}: { props: { text: string } } & BoxProps) {
+type MainProps = {
+  text: string;
+} & BoxProps;
+
+export default function Main({ text, ...restProps }: MainProps) {
+  const [glitching, setGlitching] = useState(false);
+
+  useEffect(() => {
+    let timeout: number;
+
+    const triggerGlitch = () => {
+      setGlitching(true);
+      timeout = window.setTimeout(
+        () => {
+          setGlitching(false);
+          timeout = window.setTimeout(
+            triggerGlitch,
+            1000 + Math.random() * 3000
+          );
+        },
+        200 + Math.random() * 200
+      );
+    };
+
+    triggerGlitch();
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <Box
-      className={classes.glitch}
-      data-text={props.text}
-      lts={2}
+      className={`${classes.glitch} ${glitching ? classes.glitching : ''}`}
+      data-text={text} // <-- needed for ::before & ::after
       {...restProps}
     >
-      {props.text}
+      <span className={classes.ghostX} aria-hidden="true">
+        {text}
+      </span>
+      {text}
     </Box>
   );
 }
