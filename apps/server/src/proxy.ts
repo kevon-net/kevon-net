d419f59051def5956831b6dfc4d2b33010e2b4f6
Kevon <dev@pro.kevon.net>
2025-11-12 12:11:45 +0300

    fix: change preflight response status to 200

--- apps/server/src/proxy.ts
+++ apps/server/src/proxy.ts
@@ -3,10 +3,10 @@ import { updateSession } from './libraries/supabase/middleware';
 import { setCorsHeaders } from '@repo/utilities/middeware';
 import { crossOrigins } from '@repo/constants/hosts';
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // Handle preflight
   if (request.method === 'OPTIONS') {
-    const response = NextResponse.json({}, { status: 204 });
+    const response = NextResponse.json({}, { status: 200 });
     setCorsHeaders({ crossOrigins, request, response });
     return response;
   }
