ab7a17752a2c1a90a438ef61e6298a73923089b9
kevon <work.kevon@gmail.com>
2026-06-04 13:26:54 +0300

    refactor: update metadata title in layout and add metadata to contact page

--- apps/client-web/src/app/(marketing)/contact/page.tsx
+++ apps/client-web/src/app/(marketing)/contact/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import LayoutPage from '@repo/components/layout/page';
 import PartialPageContact from '@/components/partial/page/contact';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: `Contact`,
+};
 
 export default async function Contact() {
   return (
